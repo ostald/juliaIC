9d618f15427170d7d2c4cab85c87c8ce81a658f8
OS <oli.st@live.com>
2025-03-07 12:28:45 +0000

    important bug fix: O2+ and N2+ productions were swapped

--- etiennes_project.jl
+++ etiennes_project.jl
@@ -7,6 +7,17 @@ include("get_msis.jl")
 using .ionchem
 using Dates
 
+include("interpolate_temp.jl")
+include("chemistry.jl");
+
+using DifferentialEquations
+
+#load reactions, define particles etc.
+path_reactions_file = "test_data/Reaction rates full set ext.txt"
+dndt, particles, reactions, ode_raw, dndt_str, reactions_str = chemistry.initIC(path_reactions_file)
+rrates = [r[4] for r in reactions]
+
+
 
 path_to_ion_rates = "/mnt/data/etienne/Julia/AURORA.jl/data/Visions2/Alfven_train_474s/Qzt_all_L.mat"
 path_to_neutral_atm = "/mnt/data/etienne/Julia/AURORA.jl/data/Visions2/Alfven_train_474s/neutral_atm.mat"
@@ -14,26 +25,18 @@ path_to_neutral_atm = "/mnt/data/etienne/Julia/AURORA.jl/data/Visions2/Alfven_tr
 data = matread(path_to_ion_rates)
 total_ionization = data["QO2i"] + data["QOi"] + data["QN2i"]
 total_ionization = total_ionization * 1e4 # to get units of (/m³/s), because I messed up things a bit
-z = data["h_atm"] # in (m)
-t = data["t"] # in (s)
+h = data["h_atm"] # in (m)
+ts = data["t"] # in (s)
 
-heatmap(t, z./1e3, max.(6, log10.(total_ionization)))
+heatmap(ts, z./1e3, max.(6, log10.(total_ionization)))
 
-ts = t
-te = t .+ 0.001
-ts[1] = ts[1] - 60*30 #set first timestep 30 min in past
-dt = 0.0001
-
-h = z
-
-e_prod = total_ionization';
 
 #Te, Tn from neutral_atm.mat
 neutral_atm =  matread(path_to_neutral_atm)
 Te = neutral_atm["Te"]
 Tn = neutral_atm["Tn"]
 Ti = Tn
-
+T = [Te, Ti, Tn]
 
 ne  = neutral_atm["ne"]
 nO  = neutral_atm["nO"]
@@ -41,30 +44,16 @@ nO2 = neutral_atm["nO2"]
 nN2 = neutral_atm["nN2"]
 nion = stack([ne, nN2, nO2, nO])'#, nAr, nNOp, nO2p, nOp]
 
-loc = [76, 5] #hardcoded :(
-
-t_itp = sort([ts .+ dt; te .- dt])
-e_prod = repeat(e_prod, inner = (2, 1))
-T = permutedims(repeat(stack([Te, Ti, Tn]), outer = (1, 1, length(t_itp))), (3, 1 ,2))
-
-function temp_f(dummy, t)
-    return [Te, Ti, Tn]
-end
-
 nh = length(h)
 nt = length(ts)
 np = length(ionchem.particles)
 n0 = zeros(np, nh)
 
-#interpolation timesteps:
-#t_itp = [ts[1]; (ts[2:end-1] + te[2:end-1])./2; te[end]]
-
-particles = ionchem.particles
 #assign densities
 #nion = [ne, nN2, nO2, nO, nAr, nNOp, nO2p, nOp]
 nion_mapping = ["e-", "N2", "O2", "O"]#, "Ar", "NO+", "O2+", "O+(4S)"] #mapping must correspond to ionchem.particle names
 for (ind1, ion) in enumerate(nion_mapping)
-    ind2 = findfirst(==(ion), [p[2] for p in ionchem.particles])
+    ind2 = findfirst(==(ion), [p[2] for p in particles])
     if nothing == ind2 println(ion * " not found")
     #assign initial densities
     else n0[ind2, :] = nion[ind1, :]
@@ -75,21 +64,38 @@ n0[findall(p -> p[2] == "NO+", particles)[1], :] = 1/3 .* ne
 n0[findall(p -> p[2] == "O2+", particles)[1], :] = 1/3 .* ne
 n0[findall(p -> p[2] == "O+(4S)", particles)[1], :] = 1/3 .* ne
 
+loc = [76, 5] #hardcoded :(
 
-#interpolate temperatures
-temp_itp = interpolate_temp(t_itp, T)
+e_prod = total_ionization';
+ni_prod = Array{Any}(undef, length(particles))
+i_O  = findall(p -> p[2] == "O", particles)[1]
+i_O2 = findall(p -> p[2] == "O2", particles)[1]
+i_N2 = findall(p -> p[2] == "N2", particles)[1]
+ni_prod[findall(p -> p[2] == "e-", particles)[1]]     = e_prod
+ni_prod[findall(p -> p[2] == "O+(4S)", particles)[1]] = e_prod .* n0[i_O , :]*0.56 ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
+ni_prod[findall(p -> p[2] == "O2+", particles)[1]]    = e_prod .* n0[i_O , :]*0.56 ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
+ni_prod[findall(p -> p[2] == "N2+", particles)[1]]    = e_prod .* n0[i_O , :]*0.56 ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
 
-#interpolate production and assign production rates to species
-e_prod_itp = interpolate_q(t_itp, e_prod)
-ni_prod = assign_prod(e_prod_f, e_prod_itp, ionchem.particles, n0)
-#integration period
+X = ones(nh)
+rr = [r(T, X) for r in rrates]
 
-tspan = (t_itp[1], t_itp[end])
-#ts, te, h, nion, T, e_prod, loc = getparams(con)
+tspan = (ts[1], ts[end])
 
-@time sol = ionchem.ic(tspan, n0, ni_prod, temp_itp, nh, (ts + te)./2)
+cb_f = []
+t_cb = []
+cb = PresetTimeCallback(t_cb, cb_f)
 
- #shift first timestep back half an hour
+function myODEf(dn, n, p, t)
+    ni_prod, dndt_, rr = p
+    for j in axes(n, 1)
+        dn[j, :] .= dndt_[j](ni_prod, rr, 0, n, t)
+        #        return (nprodd, rr, tem, nn, tt) -> Base.invokelatest(f, nprodd, rr, tem, nn, tt)
+    end
+    nothing
+end
 
- #no interpolation as of now
+
+prob = ODEProblem(myODEf, n0, tspan, (ni_prod, dndt, rr))
+sol = solve(prob, TRBDF2(autodiff=false), reltol = 1e-7, abstol = 1e-3, saveat = ts, callback = cb);
+return sol
 

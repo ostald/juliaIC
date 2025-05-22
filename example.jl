03fe6a491316ce63788d3010fab838aed28f7f2a
Oliver Stalder <ostalder@outlook.com>
2025-05-22 13:43:20 +0000

    updating example with nice plots

--- example.jl
+++ example.jl
@@ -1,4 +1,4 @@
-using BenchmarkTools
+#using BenchmarkTools
 
 include("loadElspec.jl");
 include("interpolate_temp.jl")
@@ -25,7 +25,9 @@ n0 = zeros(np, nh)
 t_itp = [ts[1]; (ts[2:end-1] + te[2:end-1])./2; te[end]]
 
 #assign densities
+#nion contains:
 #nion = [ne, nN2, nO2, nO, nAr, nNOp, nO2p, nOp]
+# translate that ordering to names in ionchem.partcile:
 nion_mapping = ["e-", "N2", "O2", "O", "Ar", "NO+", "O2+", "O+(4S)"] #mapping must correspond to ionchem.particle names
 for (ind1, ion) in enumerate(nion_mapping)
     ind2 = findfirst(==(ion), [p[2] for p in ionchem.particles])
@@ -46,17 +48,54 @@ ni_prod = assign_prod(e_prod_f, e_prod_itp, ionchem.particles, n0)
 tspan = (ts[1], te[end])
 #tspan = (0, 10)
 
-@time sol = ionchem.ic(tspan, n0, ni_prod, temp_itp, nh, (ts + te)./2)
+sol = ionchem.ic(tspan, n0, ni_prod, temp_itp, nh, (ts + te)./2)
 
 ni = stack(sol.u, dims =1)
 
-#be carefule; plots can be generated without transposing, but will look wierd
+##
+
+#be careful; plots can be generated without transposing, but will look wierd
 using CairoMakie
-fig, ax, hm = heatmap(sol.t, h, ni[:, 2, :])
-Colorbar(fig)
-heatmap(ts, h, e_prod)
+const cm = CairoMakie
+cm.set_theme!(Theme(colormap = :hawaii))
 
+##
 
+# Electron desnity in time and height
+fig = cm.Figure()
+ax = cm.Axis(fig[1, 1], 
+            xlabel="Time [s]", 
+            ylabel="Height [km]"
+            )
+hm = cm.heatmap!(sol.t.-sol.t[1],
+                h/1e3,
+                ni[:, 2, :], 
+                #colorscale = log10, 
+                #colorrange = (1e5, 1e11)
+                )
+cb = cm.Colorbar(fig[1, 2], 
+                hm, 
+                label = "Electron Density [m⁻³]")
+cm.display(fig)
+
+##
+
+# Ionization rate in time and height
+fig, ax, hm = cm.heatmap(ts.-ts[1],
+                        h/1e3, 
+                        e_prod,
+                        colorscale = log10, 
+                        colorrange = (10^(9.5), 1e11),
+                        axis=(xlabel="Time [s]", 
+                            ylabel="Height [km]",
+                            )
+                        )
+cb = cm.Colorbar(fig[1, 2], 
+                    hm,
+                    label="Ionization rate [m⁻³ s⁻¹]")
+cm.display(fig)
+
+##
 
 #plot!(range(ts[50],te[100],step=1e-2), [stepf(e_prod[1, :], t, ts, te) for t in range(ts[50],te[100],step=1e-2)])
 #plot!(ts[50:100], e_prod[1, 50:100])

d5a31cbe39d8a6a383712cd944b940a235e5419a
Oliver Stalder <ostalder@outlook.com>
2025-05-23 09:37:23 +0000

    evaluation of  alfven train

--- etiennes_project.jl
+++ etiennes_project.jl
@@ -11,6 +11,9 @@ include("interpolate_temp.jl")
 include("chemistry.jl");
 
 using DifferentialEquations
+using CairoMakie
+cm = CairoMakie
+
 
 #load reactions, define particles etc.
 path_reactions_file = "test_data/Reaction rates full set ext.txt"
@@ -26,6 +29,8 @@ total_ionization = total_ionization[1:end-3, :] * 1e4 # to get units of (/m³/s)
 h = data["h_atm"][1:end-3] # in (m)
 ts = data["t"] # in (s)
 
+#ts[1] = ts[1] - 1800
+
 heatmap(ts, h./1e3, max.(6, log10.(total_ionization)))#, xlimits=(0,0.1))
 
 
@@ -35,6 +40,7 @@ Te = neutral_atm["Te"][1:end-3]
 Tn = neutral_atm["Tn"][1:end-3]
 Ti = Tn
 T = [Te, Ti, Tn]
+
 
 ne  = neutral_atm["ne"][1:end-3]
 nO  = neutral_atm["nO"][1:end-3]
@@ -46,6 +52,8 @@ nh = length(h)
 nt = length(ts)
 np = length(ionchem.particles)
 n0 = zeros(np, nh)
+
+#dt = 0.0001
 
 #assign densities
 #nion = [ne, nN2, nO2, nO, nAr, nNOp, nO2p, nOp]
@@ -64,16 +72,26 @@ n0[findall(p -> p[2] == "O+(4S)", particles)[1], :] = 1/3 .* ne
 
 loc = [76, 5] #hardcoded :(
 
+
+#fix prodiction!!
 e_prod = total_ionization';
+e_prod[1, :] = e_prod[60, :]
+#e_prod_itp = [Interpolator(ts, e_prod[:, ih]) for ih in axes(e_prod, 2)]
+
 ni_prod = Array{Any}(undef, length(particles))
+
+zerof = gen_zerof(nh)
+ni_prod.= zerof
+
 i_O  = findall(p -> p[2] == "O", particles)[1]
 i_O2 = findall(p -> p[2] == "O2", particles)[1]
 i_N2 = findall(p -> p[2] == "N2", particles)[1]
-ni_prod .= 0
-ni_prod[findall(p -> p[2] == "e-", particles)[1]]     = e_prod[1, :]
-ni_prod[findall(p -> p[2] == "O+(4S)", particles)[1]] = e_prod[1, :] .* n0[i_O , :]*0.56 ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
-ni_prod[findall(p -> p[2] == "O2+", particles)[1]]    = e_prod[1, :] .* n0[i_O2, :]      ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
-ni_prod[findall(p -> p[2] == "N2+", particles)[1]]    = e_prod[1, :] .* n0[i_N2, :]*0.92 ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
+
+
+ni_prod[findall(p -> p[2] == "e-", particles)[1]]     = (t) -> e_prod[1, :]
+ni_prod[findall(p -> p[2] == "O+(4S)", particles)[1]] = (t) -> e_prod[1, :] .* n0[i_O , :]*0.56 ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
+ni_prod[findall(p -> p[2] == "O2+", particles)[1]]    = (t) -> e_prod[1, :] .* n0[i_O2, :]      ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
+ni_prod[findall(p -> p[2] == "N2+", particles)[1]]    = (t) -> e_prod[1, :] .* n0[i_N2, :]*0.92 ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
 
 
 X = ones(nh)
@@ -84,10 +102,10 @@ tspan = (ts[1], ts[end])
 #cb_f = []
 function cb_f(integrator)
     ind = findfirst(ts .>= integrator.t)
-    ni_prod[findall(p -> p[2] == "e-", particles)[1]]     = e_prod[ind, :]
-    ni_prod[findall(p -> p[2] == "O+(4S)", particles)[1]] = e_prod[ind, :] .* n0[i_O , :]*0.56 ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
-    ni_prod[findall(p -> p[2] == "O2+", particles)[1]]    = e_prod[ind, :] .* n0[i_O2, :]      ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
-    ni_prod[findall(p -> p[2] == "N2+", particles)[1]]    = e_prod[ind, :] .* n0[i_N2, :]*0.92 ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
+    ni_prod[findall(p -> p[2] == "e-", particles)[1]]     = (t) -> e_prod[ind, :]
+    ni_prod[findall(p -> p[2] == "O+(4S)", particles)[1]] = (t) -> e_prod[ind, :] .* n0[i_O , :]*0.56 ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
+    ni_prod[findall(p -> p[2] == "O2+", particles)[1]]    = (t) -> e_prod[ind, :] .* n0[i_O2, :]      ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
+    ni_prod[findall(p -> p[2] == "N2+", particles)[1]]    = (t) -> e_prod[ind, :] .* n0[i_N2, :]*0.92 ./(n0[i_O, :]*0.56 .+ n0[i_N2, :]*0.92 .+ n0[i_O2, :])
     integrator.p = ni_prod, dndt, rr
     return integrator.p
 end
@@ -109,9 +127,51 @@ prob = ODEProblem(myODEf, n0, tspan, (ni_prod, dndt, rr))
 sol = solve(prob, TRBDF2(autodiff=false), reltol = 1e-7, abstol = 1e-3, saveat = ts, callback = cb);
 #return sol
 
+ni = stack(sol.u, dims =1)
 
-function k(B, nu, amu)
-    return e .* B ./ (nu .* amu)
-end
+using JLD2
+resdir = "/mnt/data/etienne/Julia/AURORA.jl/data/Visions2/Alfven_train_474s/"
+jldsave(joinpath(resdir, "ic_coldIonosphere"*string(iter)*".jld2"); ts, h, T, e_prod, ni, particles)
 
-#function 
+
+
+cm.set_theme!(Theme(colormap = :hawaii))
+
+
+# Electron desnity in time and height
+fig, ax, hm = cm.heatmap(sol.t,
+                h/1e3,
+                ni[:, 2, :], 
+                axis=(xlabel="Time [s]", 
+                        ylabel="Height [km]",
+                    )
+                #colorscale = log10, 
+                #colorrange = (1e5, 1e11)
+                )
+cb = cm.Colorbar(fig[1, 2], 
+                hm, 
+                label = "Electron Density [m⁻³]")
+cm.display(fig)
+
+##
+
+# Ionization rate in time and height
+fig, ax, hm = cm.heatmap(ts,
+                        h/1e3, 
+                        e_prod,
+                        colorscale = log10, 
+                        colorrange = (10^(6), 1e11),
+                        axis=(xlabel="Time [s]", 
+                            ylabel="Height [km]",
+                            )
+                        )
+cb = cm.Colorbar(fig[1, 2], 
+                    hm,
+                    label="Ionization rate [m⁻³ s⁻¹]"
+                )
+cm.display(fig)
+
+##
+
+
+heatmap(sol.t, h./1e3, log10.(ni[:, 2, :]'))
